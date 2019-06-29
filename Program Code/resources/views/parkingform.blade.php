979eb71c0a5e21eacfff0e2ebc96d666d6f6defa
MusfiqurRahman01 <51596372+MusfiqurRahman01@users.noreply.github.com>
2019-06-30 00:02:45 +0600

    parkingform.php with required document field updated

--- Program Code/resources/views/parkingform.blade.php
+++ Program Code/resources/views/parkingform.blade.php
@@ -119,6 +119,16 @@
    </div>
  </div>
 
+ <div class="input-group mb-3">
+   <div class="input-group-prepend">
+     <span class="input-group-text">ID card Photocopy</span>
+   </div>
+   <div class="custom-file">
+     <input type="file" class="custom-file-input" id="inputGroupFile01">
+     <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+   </div>
+ </div>
+
 
 
 
