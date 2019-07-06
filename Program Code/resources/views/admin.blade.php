988bdda259787f7cf65d2f1a2e1cf9fbc4ae8d60
MusfiqurRahman01 <51596372+MusfiqurRahman01@users.noreply.github.com>
2019-07-06 23:43:38 +0600

    solving Issue#13 : add navbar

---
+++ Program Code/resources/views/admin.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ADMIN PANEL</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+
+
+    </head>
+    <body>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark  ">
+
+                            <a class="navbar-brand" href="#">NPS</a>
+                            <button class="navbar-toggler" data-toggle="collapse" data-target="#myNav">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse justify-content-center" id="myNav">
+                                <div class="navbar-nav">
+                                        <a href="#" class="nav-item nav-link">Home</a>
+                                        <a href="#" class="nav-item nav-link">About Us</a>
+                                        <a href="#" class="nav-item nav-link">Contact Us</a>
+                                </div>
+
+
+                            </div>
+                            <div class="navbar-nav">
+                                <form class="form-inline">
+                                    <div class="form-group mb-2">
+                                        <label for="email1" class="sr-only">Email</label>
+                                        <input type="text"  class="form-control" id="email1" value="email@example.com">
+                                      </div>
+                                      <div class="form-group mx-sm-3 mb-2">
+                                          <label for="inputPassword2" class="sr-only">Password</label>
+                                          <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+                                        </div>
+                                        <button type="submit" class="btn btn-primary mb-2">Log In</button>
+                                </form>
+
+
+                            </div>
+
+
+                </nav>
+
+
+
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+                <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    </body>
+</html>
