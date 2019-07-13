dc13203b4d57c016b35b8014089cffdee3ad7bdc
MusfiqurRahman01 <51596372+MusfiqurRahman01@users.noreply.github.com>
2019-07-13 22:38:44 +0600

    issue #18  : upade in head tag

--- Program Code/resources/views/security.blade.php
+++ Program Code/resources/views/security.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
 
     <title>Nsu Security Dept</title>
-
-
-
-
   </head>
+
+  
  <div class="container-fluid">
   <body>
 
