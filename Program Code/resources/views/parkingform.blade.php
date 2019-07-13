65e4a71008b5f781b57d5771cb04747366718ca0
MusfiqurRahman01 <51596372+MusfiqurRahman01@users.noreply.github.com>
2019-07-13 21:54:43 +0600

    Issue #17  : update head function
    update head function

--- Program Code/resources/views/parkingform.blade.php
+++ Program Code/resources/views/parkingform.blade.php
@@ -1,12 +1,14 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
     <title>NSU PARKING REGISTRATION FORM</title>
   </head>
