fba471ebba648f2cd5e4f766ea6e60f9ec1fbcac
MusfiqurRahman01 <51596372+MusfiqurRahman01@users.noreply.github.com>
2019-07-13 23:22:57 +0600

    issue #21  :   add ratio button staff
    add ratio button staff

--- Program Code/resources/views/security.blade.php
+++ Program Code/resources/views/security.blade.php
@@ -82,6 +82,15 @@
      </label>
    </div>
 
+   <div class="form-check">
+
+     <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+     <label class="form-check-label" for="exampleRadios1">
+       STAFF
+     </label>
+   </div>
+
+
 
 
   </div>
