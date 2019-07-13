97149f9ce84af4ad99ed260ff979bf7a47bb85a0
MusfiqurRahman01 <51596372+MusfiqurRahman01@users.noreply.github.com>
2019-07-13 23:21:14 +0600

    issue #21  : add ratio button student
    add ratio button student

--- Program Code/resources/views/security.blade.php
+++ Program Code/resources/views/security.blade.php
@@ -74,7 +74,13 @@
      </label>
    </div>
 
+   <div class="form-check">
 
+     <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+     <label class="form-check-label" for="exampleRadios1">
+       STUDENT
+     </label>
+   </div>
 
 
 
