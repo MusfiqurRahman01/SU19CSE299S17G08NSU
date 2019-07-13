9a12b02172dac2cfcd922597b92ccb2099f7b468
MusfiqurRahman01 <51596372+MusfiqurRahman01@users.noreply.github.com>
2019-07-14 00:01:40 +0600

    Issue #16  :  update in head tag
    add some code in head tag

--- Program Code/resources/views/about.blade.php
+++ Program Code/resources/views/about.blade.php
@@ -2,7 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
 
-        
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <title>NSU Parking System</title>
+
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+      <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
 
     </head>
 
