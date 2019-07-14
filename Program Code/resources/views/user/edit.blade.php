3534110419f7df9bd6a5270acb3e6f266a57bcc1
kh.Sadik <kh.Sadik@northsouth.edu>
2019-07-15 03:35:13 +0600

    Feature #24 : Manage users data
    editing user data

--- Program Code/resources/views/user/edit.blade.php
+++ Program Code/resources/views/user/edit.blade.php
@@ -9,5 +9,16 @@
     <h1>
         {{$user->id}}
     </h1>
+    <form class="form">
+
+            <div class="form-group mb-2">
+                    <label for="email1" class="sr-only">Email</label>
+                    <input type="text"  class="form-control" id="email1" value="{{$user->email}}">
+            </div>
+            <div class="form-group mb-2">
+                    <label for="name" class="sr-only">Email</label>
+                    <input type="text"  class="form-control" id="name" value="{{$user->first_name}}">
+            </div>             
+    </form>
 </body>
 </html>
