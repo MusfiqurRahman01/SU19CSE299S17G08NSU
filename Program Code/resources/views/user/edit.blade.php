962720c3af8e926cf2e49bc57b1e57d50b552802
kh.Sadik <kh.Sadik@northsouth.edu>
2019-07-21 21:29:43 +0600

    Feature #24 : Manage users data
    style the edit data

--- Program Code/resources/views/user/edit.blade.php
+++ Program Code/resources/views/user/edit.blade.php
@@ -3,13 +3,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <title>Nps</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
-    <h1>
-        Edit data
-    </h1>
+    
     <div class="container">
+                <h1 class="mb-4 text-center">
+                        Edit {{$user->first_name}} data
+                </h1>
                 <form class="form">
             
 
