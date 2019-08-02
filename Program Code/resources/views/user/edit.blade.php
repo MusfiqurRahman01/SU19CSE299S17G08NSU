a3f4a9e59056ff0c7009f20e8104e5b7c5b9148f
kh.Sadik <kh.Sadik@northsouth.edu>
2019-08-02 23:57:42 +0600

    Issue #34 : Fix update user data
    adding name attribute for first and last name

--- Program Code/resources/views/user/edit.blade.php
+++ Program Code/resources/views/user/edit.blade.php
@@ -26,11 +26,11 @@
                                 </div>
                                 <div class="form-group mb-2">
                                         <label for="name" class="sr-only">First Name</label>
-                                        <input type="text"  class="form-control" id="fname" value="{{$user->first_name}}">
+                                        <input type="text"  class="form-control" id="fname" name="first_name" value="{{$user->first_name}}">
                                 </div>             
                                 <div class="form-group mb-2">
                                         <label for="name" class="sr-only">Last Name</label>
-                                        <input type="text"  class="form-control" id="lname" value="{{$user->last_name}}">
+                                        <input type="text"  class="form-control" id="lname" name="last_name" value="{{$user->last_name}}">
                                 </div> 
                                 
                                 <button type="submit" class="btn btn-primary mb-2">Update</button>   
