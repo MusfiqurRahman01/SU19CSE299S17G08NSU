08a9bd4830293eb80b09593b06b42252dd198f50
kh.Sadik <kh.Sadik@northsouth.edu>
2019-08-03 23:25:06 +0600

    Issue #34 : Fix update user data
    adding span for the icon

--- Program Code/resources/views/user/edit.blade.php
+++ Program Code/resources/views/user/edit.blade.php
@@ -13,7 +13,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark  ">
                 <a class="navbar-brand" href="#">NPS</a>
                 <button class="navbar-toggler" data-toggle="collapse" data-target="#myNav">
-                        
+                        <span class="navbar-toggler-icon"></span>
                 </button>
     </nav>
     
