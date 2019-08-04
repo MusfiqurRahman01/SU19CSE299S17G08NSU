d7b1c139e375f2f38622b58e03f45086b3316fae
MusfiqurRahman01 <51596372+MusfiqurRahman01@users.noreply.github.com>
2019-08-04 18:35:07 +0600

    Feature #35 : add html tags
    add head and body tag

---
+++ Program Code/resources/views/payment.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+
+      
+
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+      <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+
+    </head>
+
+
+    <body>
+
+
+    </body>
+</html>
