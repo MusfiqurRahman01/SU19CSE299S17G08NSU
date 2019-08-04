0b836be87da9158d92ed0ebbf15e1ff651513c76
MusfiqurRahman01 <51596372+MusfiqurRahman01@users.noreply.github.com>
2019-08-04 21:03:30 +0600

    Feature #35 : Delete some unnecessary tags
    dele div class container div class row  and div class paymentcont

--- Program Code/resources/views/payment.blade.php
+++ Program Code/resources/views/payment.blade.php
@@ -66,54 +66,24 @@
                <p class="lead text-center "> This is the payment section of NSU parking system</p>
              </div>
            </div>
-         </div>
 
 
 
 
-           <div class="container">
-             <div class="row">
-               <div class="paymentCont">
+<article class="card">
 
-                 <div class="headingWrap">
-								<h3 class="headingTop text-center">Select Your Payment Method</h3>
-								</div>
+ <div class="card-body p-5">
 
-                 	<div class="paymentWrap">
-                    <div class="btn-group paymentBtnGroup btn-group-justified" data-toggle="buttons">
+<ul class="nav bg-light nav-pills rounded nav-fill mb-3" role="tablist">
+	
+</ul>
 
-                      <label class="btn paymentMethod active">
-					            	<div class="method visa"></div>
-					                <input type="radio" name="options" checked>
-					            </label>
-
-                      <label class="btn paymentMethod">
-					            	<div class="method master-card"></div>
-					                <input type="radio" name="options">
-					            </label>
+</div>
 
 
 
-
-                    </div>
-                  </div>
-
-
-
-
-
-
-               </div>
-             </div>
-           </div>
-
-
-
-
-
-
-
-
+</article>
+</div>
 
 
 
@@ -121,6 +91,6 @@
            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-             </body>
+
     </body>
 </html>
